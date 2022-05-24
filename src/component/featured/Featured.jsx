816c12edb4ee94cca003b4a8fd004849278feedb
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-05-24 13:49:12 -0400

    Working on Featured and installed react-circular-progressbar

--- src/component/featured/Featured.jsx
+++ src/component/featured/Featured.jsx
@@ -1,7 +1,21 @@
 import "./Featured.scss";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
 
 const Featured = () => {
-  return <div className="featured"> Featured</div>;
+  return (
+    <div className="featured">
+      <div className="featured-top">
+        <h1 className="featured-title">Total Revenue</h1>
+        <MoreVertIcon fontSize="small" />
+      </div>
+
+      <div className="featured-bottom">
+        <div className="featured-chart"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
