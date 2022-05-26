b8a669cc776c2b0f5c82bea030c6c25d052d793a
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-05-26 19:06:20 -0400

    Change table list from using material ui to html table instead,

--- src/component/tableList/TableList.jsx
+++ src/component/tableList/TableList.jsx
@@ -67,43 +67,76 @@ const TableList = () => {
   ];
 
   return (
-    <TableContainer component={Paper} className="table">
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell className="tableCell">Tracking ID</TableCell>
-            <TableCell className="tableCell">Product</TableCell>
-            <TableCell className="tableCell">Customer</TableCell>
-            <TableCell className="tableCell">Date</TableCell>
-            <TableCell className="tableCell">Amount</TableCell>
-            <TableCell className="tableCell">Payment Method</TableCell>
-            <TableCell className="tableCell">Status</TableCell>
-          </TableRow>
-        </TableHead>
-
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell className="tableCell">{row.id}</TableCell>
-              <TableCell className="tableCell">
-                <div className="cellWrapper">
-                  <img src={row.img} alt="" className="image" />
-                  {row.product}
-                </div>
-              </TableCell>
-              <TableCell className="tableCell">{row.customer}</TableCell>
-              <TableCell className="tableCell">{row.date}</TableCell>
-              <TableCell className="tableCell">{row.amount}</TableCell>
-              <TableCell className="tableCell">{row.method}</TableCell>
-              <TableCell className="tableCell">
-                {/* {`status ${row.status} this will change color according to its status */}
-                <span className={`status ${row.status}`}>{row.status}</span>
-              </TableCell>
+    <>
+      <TableContainer component={Paper} className="table">
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell className="tableCell">Tracking ID</TableCell>
+              <TableCell className="tableCell">Product</TableCell>
+              <TableCell className="tableCell">Customer</TableCell>
+              <TableCell className="tableCell">Date</TableCell>
+              <TableCell className="tableCell">Amount</TableCell>
+              <TableCell className="tableCell">Payment Method</TableCell>
+              <TableCell className="tableCell">Status</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell className="tableCell">{row.id}</TableCell>
+                <TableCell className="tableCell">
+                  <div className="cellWrapper">
+                    <img src={row.img} alt="" className="image" />
+                    {row.product}
+                  </div>
+                </TableCell>
+                <TableCell className="tableCell">{row.customer}</TableCell>
+                <TableCell className="tableCell">{row.date}</TableCell>
+                <TableCell className="tableCell">{row.amount}</TableCell>
+                <TableCell className="tableCell">{row.method}</TableCell>
+                <TableCell className="tableCell">
+                  {/* {`status ${row.status} this will change color according to its status */}
+                  <span className={`status ${row.status}`}>{row.status}</span>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {/* <table className="table">
+        <tr>
+          <th className="tableCell">Tracking ID</th>
+          <th className="tableCell">Product</th>
+          <th className="tableCell">Customer</th>
+          <th className="tableCell">Date</th>
+          <th className="tableCell">Amount</th>
+          <th className="tableCell">Payment Method</th>
+          <th className="tableCell">Status</th>
+        </tr>
+        {rows.map((row) => (
+          <tr key={row.id}>
+            <td className="tableCell">{row.id}</td>
+            <td className="tableCell">
+              <div className="cellWrapper">
+                <img src={row.img} alt="" className="image" />
+                {row.product}
+              </div>
+            </td>
+
+            <td className="tableCell">{row.customer}</td>
+            <td className="tableCell">{row.date}</td>
+            <td className="tableCell">{row.amount}</td>
+            <td className="tableCell">{row.method}</td>
+            <td className="tableCell">
+             
+              <span className={`status ${row.status}`}>{row.status}</span>
+            </td>
+          </tr>
+        ))}
+      </table> */}
+    </>
   );
 };
 
