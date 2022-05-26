eed9776e0e69c1b2a3fe9185e1fb49cf8943cee1
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-05-26 18:27:42 -0400

    created Table component

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,4 +1,15 @@
+import Sidebar from "../../component/sidebar/Sidebar";
+import Header from "../../component/header/Header";
+
 const List = () => {
-  return <div className="list_container">Home</div>;
+  return (
+    <div className="list">
+      <Sidebar />
+
+      <div className="list-container">
+        <Header />
+      </div>
+    </div>
+  );
 };
 export default List;
